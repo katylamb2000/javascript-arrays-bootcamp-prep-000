101103e1d02c5582f1abcc5f7303e1ac31f362d4
katy@karstaway.com <katy@karstaway.com>
2018-06-26 05:55:07 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray () {
+function addElementToBeginningOfArray (food, array) {
  chocolateBars = ["twix",...chocolateBars]
   return chocolateBars
 }
