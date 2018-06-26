db99e878f13926a80b0decb3a0abab778012d69b
katy@karstaway.com <katy@karstaway.com>
2018-06-26 06:21:07 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -26,3 +26,6 @@ function accessElementInArray (array,index) {
 function destructivelyRemoveElementFromBeginningOfArray (array) {
   array.shift ()
 }
+function removeElementFromBeginningOfArray (array) {
+  array.slice(1)
+}
